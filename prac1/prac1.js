2eb946f8db108cd98c60b83e298a72a1adc938a7
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-11-23 01:48:29 -0800

    got multiple elements in the array to show up in the view

---
+++ prac1/prac1.js
@@ -0,0 +1,11 @@
+'use strict';
+var app1 = angular.module('app1',[]);
+
+app1.controller('cltr1', function TodoController($scope){
+  $scope.todos = [
+    {name:'Learning Angular', completed:false},
+    {name:'Learning Sass', completed:true},
+    {name:'Learning React', completed:true},
+    {name:'Integrating Angular with Express and Node', completed:false}
+  ];
+});
